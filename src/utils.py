0a271e703f710194e5cfa59e32a64b9d8fde6e44
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-02-10 21:54:01 +0300

    fnc_get_cards_data_is_ready_fix

--- src/utils.py
+++ src/utils.py
@@ -27,32 +27,50 @@ def get_greeting():
     return greeting
 
 
-def get_cards_data(input_date):
-    """Функция, преобразующая dataframe транзакций в список словарей сводных данных по каждой карте"""
-    input_date_obj = datetime.datetime.strptime(input_date, "%Y-%m-%d %H:%M:%S")
-    if datetime.datetime.strptime("01.01.2018 12:49:53",
-                                  "%d.%m.%Y %H:%M:%S") <= input_date_obj < datetime.datetime.strptime(
-        "31.12.2021 16:44:00", "%d.%m.%Y %H:%M:%S"):
-        data_start_month = input_date_obj.replace(day=1, hour=00, minute=00, second=00)
+def get_filtred_dict(input_date):
+    """Функция, возвращающая отфильтрованный по дате и статусу список словарей транзакций"""
+    try:
+        input_date_obj = datetime.datetime.strptime(input_date, "%Y-%m-%d %H:%M:%S")
+        if datetime.datetime.strptime("01.01.2018 12:49:53",
+                                      "%d.%m.%Y %H:%M:%S") <= input_date_obj < datetime.datetime.strptime(
+            "31.12.2021 16:44:00", "%d.%m.%Y %H:%M:%S"):
+            data_start_month = input_date_obj.replace(day=1, hour=00, minute=00, second=00)
 
-        dict_transactions = get_transactions().to_dict(orient="records")
-        dict_transactions_date = [transactions for transactions in dict_transactions if
-                                  data_start_month <= datetime.datetime.strptime(transactions.get("Дата операции", ""),
-                                                                                 "%d.%m.%Y %H:%M:%S") <= input_date_obj and transactions.get(
-                                      "Статус") == "OK"]
-        df_transactions = pd.DataFrame(dict_transactions_date)
-        not_null_df_transactions = df_transactions[["Номер карты", "Сумма операции", "Дата операции"]].loc[
-            (df_transactions["Номер карты"].notnull()) & (df_transactions["Сумма операции"].notnull()) & (
-                    df_transactions["Дата операции"].notnull() & (df_transactions["Сумма операции"] < 0))]
-        group_df_transactions = not_null_df_transactions.groupby("Номер карты")["Сумма операции"].sum()
-        dict_transactions_filtred = group_df_transactions.to_dict()
-        result = [{"last_digits": k[1:], "total_spent": abs(v), "cashback": abs(v * 0.01)} for k, v in
-                  dict_transactions_filtred.items()]
-        return result
-    else:
+            dict_transactions = get_transactions().to_dict(orient="records")
+            dict_transactions_date = [transactions for transactions in dict_transactions if
+                                      data_start_month <= datetime.datetime.strptime(
+                                          transactions.get("Дата операции", ""),
+                                          "%d.%m.%Y %H:%M:%S") <= input_date_obj and transactions.get(
+                                          "Статус") == "OK"]
+            return dict_transactions_date
+        else:
+            return [{}]
+    except Exception as e:
+        print(f"При выполнении операции произошла ошибка: {e}")
         return [{}]
+
+
+def get_cards_data(input_date):
+    """Функция, возвращающая список словарей крайних цифр, сумм расходов и сумм кэшбека по каждой карте"""
+    try:
+        df_transactions = pd.DataFrame(get_filtred_dict(input_date))
+        not_null_df_transactions = df_transactions[["Номер карты", "Сумма платежа", "Дата операции"]].loc[
+            (df_transactions["Номер карты"].notnull()) & (df_transactions["Сумма платежа"].notnull()) & (
+                    df_transactions["Дата операции"].notnull() & (df_transactions["Сумма платежа"] < 0))]
+        group_df_transactions = not_null_df_transactions.groupby("Номер карты")["Сумма платежа"].sum()
+        dict_transactions_filtred = group_df_transactions.to_dict()
+        result = [{"last_digits": k[1:], "total_spent": round(abs(v), 2), "cashback": round(abs(v * 0.01), 2)} for
+                  k, v in dict_transactions_filtred.items()]
+        return result
+    except Exception as e:
+        print(f"При выполнении операции произошла ошибка: {e}")
+        return [{}]
+
+
+def get_top_transactions():
+    pass
 
 
 if __name__ == "__main__":
     get_greeting()
-    get_cards_data("2020-05-29 10:50:03")
+    get_cards_data("2021-05-29 10:50:03")
